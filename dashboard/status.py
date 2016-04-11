6e4395f99347959fd7a456fd175d08d8f64b09b0
yjzhu1 <yimeng.zhu@mail.mcgill.ca>
2016-04-10 21:18:11 -0400

    colour updated

--- dashboard/status.py
+++ dashboard/status.py
@@ -9,6 +9,8 @@ status = form.getvalue('status')
 user = urllib.urlopen("http://cgi.cs.mcgill.ca/~eander40/currentUser.txt")
 
 print "Content-type: text/html\n\n"
+print "<html>"
+print '<body style = "background-color : #CA8E45">'
 print ' <a href="http://www.cs.mcgill.ca/~yzhu399/dashboard.py">Back to Dashboard</a>'
 
 currentUser = user.read()
@@ -17,3 +19,5 @@ if status:
 	f = open("status.txt", "a")
 	f.write(currentUser + ' ' + status + " \n\r")
 	f.close()
+print "</body>"
+print "</html>"
